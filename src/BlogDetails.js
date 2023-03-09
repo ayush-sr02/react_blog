c214790f16c946ce954c09b0448b52e7fc898b15
ayush-sr02 <ayushsh2002@gmail.com>
2023-03-09 23:01:56 +0530

    first commit

---
+++ src/BlogDetails.js
@@ -0,0 +1,31 @@
+import { useHistory, useParams } from "react-router-dom";
+import useFetch from './useFetch';
+const BlogDetails = () => {
+    const history = useHistory();
+
+    const handleDelete = () => {
+        fetch('http://localhost:8000/blogs/'+blog.id,{
+            method : 'DELETE'
+        }).then(()=>{
+            history.push('/');
+        })
+    }
+    const {id} = useParams();
+    const {data:blog,error,isPending} = useFetch(' http://localhost:8000/blogs/'+id);
+    return (
+        <div className="blog-details">
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written By {blog.author}</p>
+                    <div>{blog.body}</div>
+                    <button onClick={handleDelete}>DELETE</button>
+                </article>
+            )}
+        </div>
+    );
+}
+
+export default BlogDetails;
